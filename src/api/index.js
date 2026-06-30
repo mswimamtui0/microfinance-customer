47466c46ade12e4257132de75a98399273e65bc4
mswimamtui0 <mswimamtui0@gmail.com>
2026-06-30 07:52:49 +0300

    Debug API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+console.log('🔗 API URL:', process.env.REACT_APP_API_URL || 'http://localhost:8000/api');
 
 console.log('🔗 Customer Portal API URL:', API_URL);
 
